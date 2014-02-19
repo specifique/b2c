4a448312f10fd9df4ebe0ff7caaa2d5e20f4b0e0
Rolf de Jong <r.dejong@specifique.nl>
2014-02-19 12:08:38 +0100

    Deleted static.js
    File is replaced by mockups.js

--- static.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-  , app     = express()
-  , port    = process.env.PORT || 5000;
-
-app.configure(function() {
-  app.use('/', express.static(__dirname + '/mockups'));
-});
-
-app.listen(port);
